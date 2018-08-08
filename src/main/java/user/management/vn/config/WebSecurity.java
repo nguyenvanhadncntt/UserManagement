6a582f0fd9d0c32820e1e032bcab8ccd9070e23a
at-hanguyen2 <ha.nguyen2@asiantech.vn>
2018-08-08 11:08:02 +0700

    set properties for mail username and password

---
+++ src/main/java/user/management/vn/config/WebSecurity.java
@@ -0,0 +1,9 @@
+package user.management.vn.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+
+	
+	
+}
