a99225c725ae6ccb849db8d43969dc58d42dafcd
at-hanguyen2 <ha.nguyen2@asiantech.vn>
2018-08-20 18:05:21 +0700

    login

--- src/main/java/user/management/vn/config/WebSecurity.java
+++ src/main/java/user/management/vn/config/WebSecurity.java
@@ -1,13 +1,51 @@
 package user.management.vn.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class WebSecurity extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+		http.csrf().disable();
+		
+		http.authorizeRequests().anyRequest().authenticated();
+		
+		http.authorizeRequests()
+				.and().formLogin()
+				.loginPage("/login").usernameParameter("email").passwordParameter("password")
+				.loginProcessingUrl("/login").permitAll().defaultSuccessUrl("/	")
+				.failureUrl("/login?login_error=false").and().logout().logoutUrl("/logout")
+				.deleteCookies("JSESSIONID").logoutSuccessUrl("/login?logout").permitAll().and().httpBasic();
+//		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+	}
+
+	@Override
+	public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web)
+			throws Exception {
+		web.ignoring().antMatchers("css/**", "js/**");
 	}
 }
---
+++ src/main/java/user/management/vn/controller/LoginController.java
@@ -0,0 +1,17 @@
+package user.management.vn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping(path="/login")
+	public String login() {
+		return "login";
+	}
+	@GetMapping(path="/home")
+	public String home() {
+		return "home";
+	}
+}
