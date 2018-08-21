361fd1a8d4b2b95ece93144b7856019cbf6394f0
at-hanguyen2 <ha.nguyen2@asiantech.vn>
2018-08-21 09:10:05 +0700

    update login

--- src/main/java/user/management/vn/controller/LoginController.java
+++ src/main/java/user/management/vn/controller/LoginController.java
@@ -1,5 +1,8 @@
 package user.management.vn.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +11,10 @@ public class LoginController {
 	
 	@GetMapping(path="/login")
 	public String login() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(!(auth instanceof AnonymousAuthenticationToken)) {
+			return "redirect:/home";
+		}
 		return "login";
 	}
 	@GetMapping(path="/home")
