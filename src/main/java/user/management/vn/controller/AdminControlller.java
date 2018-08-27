03ddf296e260b52d7d7514de12b148ca00c94c16
at-hanguyen2 <ha.nguyen2@asiantech.vn>
2018-08-27 10:46:34 +0700

    fix circle bean

--- src/main/java/user/management/vn/controller/AdminControlller.java
+++ src/main/java/user/management/vn/controller/AdminControlller.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminControlller {
 	
-	@GetMapping("group/{groupId}")
+	@GetMapping("/group/{groupId}")
 	public String showUserInGroup() {
-		return "";
+		return "group";
 	}
 }
--- src/main/java/user/management/vn/entity/response/UserResponse.java
+++ src/main/java/user/management/vn/entity/response/UserResponse.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
