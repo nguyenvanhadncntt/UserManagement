fc3e5cf092b9ceb1ed6db9ebf54fc4819e003a39
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2018-11-26 17:21:30 +0700

    implement code

--- src/main/java/user/management/vn/controller/GroupChatController.java
+++ src/main/java/user/management/vn/controller/GroupChatController.java
@@ -3,6 +3,7 @@ package user.management.vn.controller;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import user.management.vn.websocket.model.ChatMessage;
@@ -10,7 +11,9 @@ import user.management.vn.websocket.model.ChatMessage;
 @Controller
 public class GroupChatController {
 	
-	@MessageMapping("/group/{groupId}")
+	
+	@MessageMapping("/{groupId}/conversation")
+	@SendTo("/group/{groupId}")
 	public ChatMessage sendMessageToGroup(@DestinationVariable(value="groupId") String groupId,
 			@Payload ChatMessage message) {
 		return message;
